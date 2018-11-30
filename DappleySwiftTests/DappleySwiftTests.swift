ef34bfd484e7795fd466ed2d9fc14a53c1bb5c76
AlexLiSong <songhongli@gmail.com>
2018-11-29 18:57:52 -0800

    Remove BigInt from tests

--- DappleySwiftTests/DappleySwiftTests.swift
+++ DappleySwiftTests/DappleySwiftTests.swift
@@ -12,7 +12,6 @@ import SwiftProtobuf
 import CryptoSwift
 import CryptoEthereumSwift
 import EthereumKit
-import BigInt
 @testable import DappleySwift
 
 class DappleySwiftTests: XCTestCase {
@@ -31,7 +30,7 @@ class DappleySwiftTests: XCTestCase {
         print(rpc.GetBlockchainInfo())
         print(rpc.GetBlockByHeight())
         rpc.GetUtxos(address: "dFQd3DCkKJ226LBVDCFanHM7c891AGxbZW")
-        var amount:BigInt = 3
+        var amount:BInt = 3
         rpc.Send(from: "dFQd3DCkKJ226LBVDCFanHM7c891AGxbZW", to: "dMjVoMPgZonQ6QKUT7efvHzUFNTT8r1qSp", amount: amount, privateKey: Data(hex: "0f5645a3a724a0e079df5f3b477da82b280d64c750a2d499291fc66b3f1deb15"))
         //Lis-MacBook-Pro:cli lisong$ ./cli send -from dFQd3DCkKJ226LBVDCFanHM7c891AGxbZW -to dMjVoMPgZonQ6QKUT7efvHzUFNTT8r1qSp -amount 1
         print("the number of blocks: \(rpc.GetBlocks())")
