1e328dc70363fb03956cbe7f67f1e52c4d7814a2
AlexLiSong <songhongli@gmail.com>
2018-12-03 17:44:17 -0800

    Add a test to deploy a smart contract

--- DappleySwiftTests/DappleySwiftTests.swift
+++ DappleySwiftTests/DappleySwiftTests.swift
@@ -15,9 +15,11 @@ import EthereumKit
 @testable import DappleySwift
 
 class DappleySwiftTests: XCTestCase {
+    private var rpc:RpcProvider = RpcProvider (host: "18.224.247.158:50050")
     
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        let rpc = RpcProvider (host: "18.224.247.158:50050")
     }
     
     override func tearDown() {
@@ -26,7 +28,6 @@ class DappleySwiftTests: XCTestCase {
     
     func testGPRC() {
         gRPC.initialize()
-        let rpc = RpcProvider (host: "127.0.0.1:50050")
         print(rpc.getBlockchainInfo())
         print(rpc.getBlockByHeight())
         rpc.getUtxos(address: "dFQd3DCkKJ226LBVDCFanHM7c891AGxbZW")
@@ -36,15 +37,18 @@ class DappleySwiftTests: XCTestCase {
         
     }
     func testGRPCSend(){
-        let rpc = RpcProvider (host: "127.0.0.1:50050")
         var amount:BInt = 3
         var parcel = Parcel(toAddress: "dMjVoMPgZonQ6QKUT7efvHzUFNTT8r1qSp", tip: 2, value: amount, contract: "")
         rpc.send(from: "dFQd3DCkKJ226LBVDCFanHM7c891AGxbZW", parcel: parcel, privateKey: Data(hex: "0f5645a3a724a0e079df5f3b477da82b280d64c750a2d499291fc66b3f1deb15"))
     }
     func testGRPCSendContract(){
-        let rpc = RpcProvider (host: "127.0.0.1:50050")
         var amount:BInt = 3
         var parcel = Parcel(toAddress: "cMjVoMPgZonQ6QKUT7efvHzUFNTT8r1qSp", tip: 2, value: amount, contract: "hello world")
+        rpc.send(from: "dFQd3DCkKJ226LBVDCFanHM7c891AGxbZW", parcel: parcel, privateKey: Data(hex: "0f5645a3a724a0e079df5f3b477da82b280d64c750a2d499291fc66b3f1deb15"))
+    }
+    func testGRPCUpdateSteps(){
+        var amount:BInt = 3
+        var parcel = Parcel(toAddress: "cctaYJtTPL1qDiQJUz967jLh6buhjV6UpB", tip: 1, value: amount, contract: "{\"function\":\"record\",\"args\":[\"dFQd3DCkKJ226LBVDCFanHM7c891AGxbZW\",\"12\"]}")
         rpc.send(from: "dFQd3DCkKJ226LBVDCFanHM7c891AGxbZW", parcel: parcel, privateKey: Data(hex: "0f5645a3a724a0e079df5f3b477da82b280d64c750a2d499291fc66b3f1deb15"))
     }
     func testPerformanceExample() {
